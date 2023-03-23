e029ab88c789c1df82d7b108c4368bb37f5c4346
Antgenix <alessandro.roethenmund@gmail.com>
2023-03-23 20:26:35 +0100

    redo

--- client/App.js
+++ client/App.js
@@ -1,30 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { RNCamera } from 'react-native-camera';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
 
-  const [isScannerOpen, setIsScannerOpen] = useState(false);
-
-  const handleScannerButtonPress = () => {
-    setIsScannerOpen(!isScannerOpen);
-  };
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={handleScannerButtonPress}>
-        <Text>Open QR Scanner</Text>
-      </TouchableOpacity>
-
-      {isScannerOpen && (
-        <RNCamera
-          style={styles.camera}
-          onBarCodeRead={(event) => {
-            console.log(event.data);
-          }}
-        />
-      )}
+      
     </View>
   );
 }
@@ -35,9 +16,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  camera: {
-    height: 200,
-    width: 200,
-  },
+  }
 });
